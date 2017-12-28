222e2334f6a13a02e949864cde68cabda45e4af7
rednesss <rednesss@users.noreply.github.com>
2017-12-28 15:06:28 +0800

    Initial commit

---
+++ src/main/java/com/cr/pojo/Person.java
@@ -0,0 +1,25 @@
+package com.cr.pojo;
+
+/**
+ * Created by y on 2017/12/27.
+ */
+public class Person {
+    private String id;
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
